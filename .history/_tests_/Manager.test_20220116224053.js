a8f09da43631c663f9a84532afa0d76120eb654a
brenhamp <bpozzi@trinity.edu>
2022-01-16 22:47:08 -0600

    fixed missing getID functions in class library

--- .history/_tests_/Manager.test_20220116224053.js
+++ .history/_tests_/Manager.test_20220116224053.js
@@ -3,7 +3,7 @@ const Manager = require ("../lib/class_manager");
 test("Can recognize office number.", () => {
     const officeNumber = "123";
     const managerObj = new Manager ("Brenham", 12345, "bpozzi@trinity.edu", officeNumber);
-    expect(managerObj.officeNumber).toBe(officeNumber);
+    expect(managerObj.officeNumber()).toBe(officeNumber);
 });
 
 test("getOfficeNumber returns office number.", () => {
