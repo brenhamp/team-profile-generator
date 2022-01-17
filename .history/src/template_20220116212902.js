82559b14243bb0f74dac06da5ce98bf4e25fb71e
brenhamp <bpozzi@trinity.edu>
2022-01-16 21:35:52 -0600

    finished html generation template

--- .history/src/template_20220116212902.js
+++ .history/src/template_20220116212902.js
@@ -93,36 +93,3 @@ const generatePage = team => {
 
     return html.join("");
 }
-
-module.exports = team => {
-    return `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles/styles.css"/>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <title>My Team</title>
-</head>
-
-<body>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="hero col-12 mb-3">
-                <h1 class="text-center">My Team</h1>
-            </div>
-        </div>
-    </div>
-    <div class="container-fluid>
-        <div class="row">
-            <div class="row col-12 d-flex-justify-content-center">
-            ${generatePage(team)}
-            </div>
-        </div>
-    </div>
-</body>
-</html>
-    `;
-};
